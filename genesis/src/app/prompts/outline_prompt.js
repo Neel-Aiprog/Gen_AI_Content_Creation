04aea946e8f79708a64e8ed16c3655aa2c536d82
Neel-aiprog <mhaskeneel0709@gmail.com>
2025-12-22 18:16:58 +0530

    deleted the earlier files

--- genesis/src/app/prompts/outline_prompt.js
+++
@@ -1,6 +0,0 @@
-import {PromptTemplate} from "@langchain/core/prompts"
-const outline_prompt=new PromptTemplate({
-    template:"generate a outline for the following text \n {text}",
-    input_variables:['text']
-})
-export default outline_prompt;
--- genesis/src/app/prompts/section_prompt.js
+++
@@ -1,6 +0,0 @@
-import {PromptTemplate} from "@langchain/core/prompts"
-const section_prompt=new PromptTemplate({
-    template:"generate sections for the following text based on the topic given in the text\n {text}",
-    input_variables:['text']
-})
-export default section_prompt;
