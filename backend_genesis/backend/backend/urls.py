5c1ee691e64e652c7b5a0bfd01208bda3c630313
NishchayMittal <mittal.nishchay1501@gmail.com>
2026-01-10 18:11:48 +0530

    Added backend info on deployed

--- backend_genesis/backend/backend/urls.py
+++ backend_genesis/backend/backend/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import JsonResponse
+
+def root(request):
+    return JsonResponse({"status": "Backend is running"})
 
 urlpatterns = [
+    path("", root),
     path("admin/", admin.site.urls),
-
-    # Main AI API (blog + images)
     path("api/", include("blog.urls")),
 ]
